75de1cd4206e9abbd5cd0f4ff59075f6315cc4be
zyns <trungluc217@gmail.com>
2020-09-25 23:21:38 +0700

    fix link http:\

--- pages/[slug].js
+++ pages/[slug].js
@@ -8,7 +8,7 @@ export default function RedirectRoot() {
         const res = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/links/${slug}`)        
         if (res.data.status == "GREEN") {
             let url = res.data.link.root;
-            if (url.slice(0, 8) != 'https://') url = 'https://' + url;
+            if (url.slice(0, 8) != 'https://' || url.slice(0, 7) != 'http://') url = 'https://' + url;
             window.location.href = url;
             return;
         } else {
